022c1cbf7d31636bf50abb769d9b3c3ba76c5e46
seraphy <seraphy@seraphyware.com>
2014-02-09 23:26:47 +0900

    SerialVersionIDの追加のみ

--- src/main/java/jp/seraphyware/sample/standaloneELContext/ImplicitContext.java
+++ src/main/java/jp/seraphyware/sample/standaloneELContext/ImplicitContext.java
@@ -6,4 +6,5 @@ import java.util.HashMap;
  * 暗黙の"implicit"コンテキストの型
  */
 class ImplicitContext extends HashMap<String, Object> {
+	private static final long serialVersionUID = -1256354618165972416L;
 }
